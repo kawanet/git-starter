ef3d9e08b2739f15d96d2172a5a7ade30977782d
kawanet <u-suke@kawa.net>
2013-03-21 07:50:23 +0900

    add tests

---
+++ test/git-starter.test.js
@@ -0,0 +1,26 @@
+// starter.test.js
+
+var expect = require('chai').expect;
+var GitStarter = require("../src/git-starter");
+
+describe('GitStarter', function() {
+
+    describe('#applyStarter', function() {
+        var starter;
+        var opts = {};
+        var args = {};
+
+        beforeEach(function() {
+            starter = new GitStarter(opts, args);
+        });
+
+        it('emit "error" on failure', function(done) {
+            starter.on("error", function(err) {
+                done();
+            });
+            starter.applyStarter();
+        });
+
+        // TODO: ADD MORE TESTS
+    });
+});
